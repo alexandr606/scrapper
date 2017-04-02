89e5b779782d2964a89dc102a8dcaa57064394d4
evhenij <baranov.evgenij@gmail.com>
2017-04-03 01:01:19 +0300

    feat: add first working version for avito parser

---
+++ catalogs/avito.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Nightmare = require('nightmare');
+const nightmare = new Nightmare({ show: true });
+const moment    = require('moment');
+
+/*
+returning values
+{
+  balance: Float,
+  expense: Integer,
+  clicks: Integer
+}
+*/
+
+let avito = async (login, password) => {
+    try {
+        let balance = await nightmare
+            .goto('https://www.avito.ru/profile/context/campaigns')
+            .type('input[name="login"]', login)
+            .type('input[name="password"]', password)
+            .click('.login-form-submit')
+            .wait('.context-balance__value')
+            .evaluate(() => {
+                return document.querySelector('.context-balance__value').innerText;
+            });
+
+        let expense = await nightmare
+            .goto('https://www.avito.ru/profile/context/stats')
+            .evaluate(() => {
+                return [...document.querySelectorAll('table.context-table tbody tr')]
+                    .map(el => el.innerText);
+            });
+
+        await nightmare.end();
+
+        let formedExpence = [];
+
+        expense.forEach( item => {
+            let formed = __formExpense(item);
+
+            let yesterday = moment().clone().subtract(1, 'days').startOf('day');
+
+            if( yesterday.diff(moment(formed.date, 'YYYY-MM-DD'), 'days') === 0 ) {
+                formedExpence = formed;
+            };
+        });
+
+        return {
+            balance : parseFloat(balance.replace(' ', '')),
+            expense : formedExpence.cost,
+            clicks  : parseInt(formedExpence.clicks, 10)
+        };
+
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+function __formExpense(data) {
+    let tempData = data.split('\t');
+    let tempDate = tempData[0].split(' ');
+
+    let date    = moment([tempDate[2], parseInt(dates[tempDate[1]]), tempDate[0]]).format('YYYY-MM-DD');
+    let cost    = parseFloat(tempData[tempData.length-1]);
+    let clicks  = tempData[2];
+
+    return {
+        clicks  : clicks,
+        date    : date,
+        cost    : cost
+    };
+};
+
+const dates = {
+    'декабря': '11',
+    'ноября': '10',
+    'октября': '09',
+    'сентября': '08',
+    'августа': '07',
+    'июля': '06',
+    'июня': '05',
+    'мая': '04',
+    'апреля': '03',
+    'марта': '02',
+    'февраля': '01',
+    'января': '00',
+};
+
+module.exports = avito;
