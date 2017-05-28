01202d626deeda9bd982191e6cd7f7008d09660a
evhenij <baranov.evgenij@gmail.com>
2017-05-28 20:38:06 +0300

    minor

--- parser.js
+++ parser.js
@@ -9,6 +9,7 @@ let avtopro         = require('./catalogs/avtopro.prices');
     let parts = [0, 500, 1000, 1500, 2000];
 
     parts.forEach( part => {
+        console.log(part)
         return LinksService.getLinks(part, function (links) {
 
             return avtopro.parsePrices(links).then( result => {
